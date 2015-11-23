8b3d605449eec3e103006f2f15d4c0b2cd130938
Kapil Kumar <Kapil@Kapil-Kumars-MacBook-Pro-2.local>
2015-11-23 16:19:34 +0530

    blog

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,18 @@
+class StaticPagesController < ApplicationController
+  
+  def home
+   
+  end
+
+  def help
+  	#render layout: 'false'
+  
+    #if you want to show the particular layout in that action make it false
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
